be4d4671815d077b04c1a6ef9e1f0a74c3f1f976
Pedro Freitas <pedromuab@gmail.com>
2026-02-19 19:40:35 -0300

    optimize imports

--- app/src/main/java/br/com/droidchat/ui/ChatApp.kt
+++ app/src/main/java/br/com/droidchat/ui/ChatApp.kt
@@ -1,10 +1,8 @@
 package br.com.droidchat.ui
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
